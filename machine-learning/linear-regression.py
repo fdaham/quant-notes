1959bcaf5cdff36241f023c5e3cd4273a537d7bb
Farrah Daham <farrahad@gmail.com>
2021-10-14 18:50:07 -0700

    Add data analysis commands notebook.

--- machine-learning/linear-regression.py
+++ machine-learning/linear-regression.py
@@ -6,13 +6,12 @@ from sklearn.model_selection import train_test_split
 
 # Extract dependent var and predictor vars
 df = pd.read_csv('50_Startups.csv')
+df = df.dropna()
 x = df.drop('Profit', axis = 1)
 y = df[['Profit']]
 
 # Handle categorical vars by turning them into dummy indicator vars
-dummy_states = pd.get_dummies(x, drop_first = True)
-x = x.drop('State', axis = 1)
-x = pd.concat([x, dummy_states], axis = 1)
+x = pd.get_dummies(x, drop_first = True)
 
 # Get predicted model parameters
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
@@ -22,6 +21,5 @@ y_pred =  model.predict(x_test)
 # Check model accuracy
 r2 = model.score(x_test, y_test)
 mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
 print('R-Squared Error = ', r2)
 print('Mean Squared Error = ', mse)
